f6df8064f3bb8f0c4d259604d06992fb3d9defd9
CKanishka <ckanishka24@gmail.com>
2020-02-01 18:22:52 +0530

    fixed local host issue

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const PORT = process.env.PORT || 5000
-const baseUrl = process.env.baseURL || "http://localhost:5000"
+const baseURL = "https://myblogdashboard.herokuapp.com/"
 export const getItems = () => dispatch => {
     dispatch(itemsLoading());
     axios
-        .get(`${baseUrl}/api`)
+        .get(`${baseURL}${PORT}/api`)
         .then(res=>{
             dispatch({
                 type:'get-items',
@@ -16,7 +16,7 @@ export const getItems = () => dispatch => {
 };
 
 export const deleteItem = id => dispatch => {
-    axios.delete(`${baseUrl}/api/${id}`).then(res =>
+    axios.delete(`${baseURL}${PORT}/api/${id}`).then(res =>
       dispatch({
         type: 'delete-item',
         payload: id
@@ -28,7 +28,7 @@ export const deleteItem = id => dispatch => {
 
 export const addItem = (item) =>dispatch=> {
     axios
-        .post(`${baseUrl}/api`,item)
+        .post(`${baseURL}${PORT}/api`,item)
         .then(res=>{
             dispatch({
                 type:'add-item',
@@ -40,7 +40,7 @@ export const addItem = (item) =>dispatch=> {
 
 export const updateItem = (item,id) =>dispatch=> {
     axios
-        .put(`${baseUrl}/api/${id}`,item)
+        .put(`${baseURL}${PORT}/api/${id}`,item)
         .then(res=>{
             dispatch({
                 type:'update-item',
