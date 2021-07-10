dfa3c3b2fae278b4e9ff602503e4862ad17a2273
CKanishka <ckanishka24@gmail.com>
2021-07-10 18:59:23 +0530

    UserListItem refactored to a separate component

---
+++ client/src/components/UserList.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { connect } from "react-redux";
+import {
+  getItems,
+  deleteItem,
+  updateItem,
+  getArticleCount,
+  getGenreCount,
+} from "../actions/itemActions";
+import UserItem from "./UserItem";
+
+class UserList extends React.Component {
+  componentDidMount() {
+    this.props.getItems(); //calling the action
+    this.props.getArticleCount();
+    this.props.getGenreCount();
+  }
+  
+  editUser = (user) => {
+    this.props.toggleShowModal(user)
+  }
+
+  deleteUser = (id) => {
+    this.props.deleteItem(id); //calling the action
+  };
+
+  render() {
+    const { items } = this.props.item;
+    return (
+      <div className="container">
+        <div className="row">
+          {items.map((user) => (
+            <div className="col-lg-3 col-md-4 col-sm-6 mb-2" key={user._id}>
+              <UserItem user={user} editUser={()=>this.editUser(user)} deleteUser={this.deleteUser}></UserItem>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+const mapStateToProps = (state) => ({
+  item: state.item, //from reducer
+});
+export default connect(mapStateToProps, {
+  getItems,
+  deleteItem,
+  updateItem,
+  getArticleCount,
+  getGenreCount,
+})(UserList);
