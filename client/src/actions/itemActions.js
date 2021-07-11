a19be1960c7aec355208c00ffea3014a184d53b5
CKanishka <ckanishka24@gmail.com>
2021-07-11 13:42:27 +0530

    Passing URL in build script

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = process.env.REACT_APP_BASE_URL || 'https://blogplanet.herokuapp.com/api';
+const baseURL = process.env.REACT_APP_BASE_URL;
 
 export const getItems = () => (dispatch) => {
   dispatch(itemsLoading());
